ef7f36433d4b2bb4e77b43798010c70ca82440eb
big <cscd053@gmail.com>
2022-05-22 22:04:18 +0900

    add : SQS 실습 (진행중)

---
+++ README.md
@@ -0,0 +1,7 @@
+# AWS Service 실습
+
+1. SQS(Simple-Queue-Service)
+   - Send Message
+   - Receive Message
+
+참고 : https://aws.amazon.com/ko/blogs/korea/getting-started-with-spring-boot-on-aws/
---
+++ simple-queue-service/src/main/java/com/example/simplequeueservice/SimpleQueueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplequeueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleQueueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleQueueServiceApplication.class, args);
+    }
+
+}
