5a1a1184d718fc5b16782353a3a379bc67a6b68b
big <cscd053@gmail.com>
2022-06-12 18:16:42 +0900

    add : Codedeploy 실습 준비

---
+++ codedeploy-test/src/main/java/com/example/codedeploytest/CodedeployTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.codedeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodedeployTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodedeployTestApplication.class, args);
+    }
+
+}
---
+++ codedeploy-test/src/test/java/com/example/codedeploytest/CodedeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codedeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedeployTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
